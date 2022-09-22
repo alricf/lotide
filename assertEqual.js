339a65c8c32164a17a1604cffd39ec616771ddb1
ACF <alricfernandes11@gmail.com>
2022-09-22 17:06:19 -0400

    Added file assertEqual.js

---
+++ assertEqual.js
@@ -0,0 +1,14 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  } else if (actual !== expected) {
+    console.log("❌ Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+
+// TEST CODE
+assertEqual("LHL", "LHL");
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual(10, 1111);
