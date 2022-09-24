61aff81231f261377fd91bfdbea5ff16187aa586
ACF <alricfernandes11@gmail.com>
2022-09-24 16:04:34 -0400

    New file that flattens array and asserts if array was flattened.

---
+++ flatten.js
@@ -0,0 +1,31 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let result = eqArrays(array1, array2);
+  if (result === true) {
+    console.log("✅ The two arrays are equal.");
+  } else {
+    console.log("❌ The two arrays are not equal.");
+  }
+};
+
+const flatten = function(arr) {
+  if (Array.isArray(arr)) {
+    let flattenArr = [].concat.apply([], [...arr]);
+    return flattenArr;
+  }
+};
+
+console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]);
