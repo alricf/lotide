30a48efc6feebd4124490694fa7d27a34911587e
ACF <alricfernandes11@gmail.com>
2022-09-24 13:45:47 -0400

    New file that sends message to see if two arrays are equal or not.

---
+++ assertArraysEqual.js
@@ -0,0 +1,28 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    }
+    return true;
+  }
+};
+
+const assertArraysEqual = function(array1, array2) {
+  let result = eqArrays(array1, array2);
+  if (result === true) {
+    console.log("✅ The two arrays are equal.");
+  } else {
+    console.log("❌ The two arrays are not equal.");
+  }
+};
+
+//TEST CODE
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 4]);
+assertArraysEqual([1, 2, 3], [1, 2, "3"]);
+assertArraysEqual([1, 2, 3], [1, 2, "abcd"]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4 ,5, 6]);
