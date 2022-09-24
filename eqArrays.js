75529b1e13018919f5e0c92c117a67a0a6003ccd
ACF <alricfernandes11@gmail.com>
2022-09-24 13:22:19 -0400

    Corrected last test case comment from should pass to should fail.

--- eqArrays.js
+++ eqArrays.js
@@ -24,4 +24,4 @@ assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, 4]), false); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), false); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [1, 2, "abcd"]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4 ,5, 6]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4 ,5, 6]), true); // => should FAIL
